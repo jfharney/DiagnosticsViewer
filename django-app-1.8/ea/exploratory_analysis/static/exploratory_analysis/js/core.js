42b3a1c365a7cf5572dadc20f95b53a40a48b464
Harney, John F <harneyjf@ornl.gov>
2015-07-13 10:41:08 -0400

    Adding static files

---
+++ django-app-1.8/ea/exploratory_analysis/static/exploratory_analysis/js/core.js
@@ -0,0 +1,5 @@
+var EA = EA || {};
+
+EA.host = 'localhost';
+EA.port = document.location.port;
+
---
+++ django-app-1.8/ea/exploratory_analysis/static/exploratory_analysis/js/security/signin.js
@@ -0,0 +1,77 @@
+$(document).ready(function(){
+
+	//for post requests, need to get the csrf token
+	function getCookie(name) {
+        var cookieValue = null;
+        if (document.cookie && document.cookie != '') {
+            var cookies = document.cookie.split(';');
+            for (var i = 0; i < cookies.length; i++) {
+                var cookie = jQuery.trim(cookies[i]);
+                // Does this cookie string begin with the name we want?
+                if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
+    var csrftoken = getCookie('csrftoken');
+    
+	console.log('csrftoken ' + csrftoken);
+	
+
+	$('#signin').click(function(){
+
+		var username = $('#username').val();
+        var password = $('#password').val();
+		var peernode = 'esg.ccs.ornl.gov';
+		
+		
+		var input_data = {
+				'csrfmiddlewaretoken': csrftoken,
+				'username':username,
+				'password':password,
+				'peernode':peernode
+		};
+		
+		var url = 'http://' + EA.host + ':' + EA.port + '/exploratory_analysis/auth/';
+		
+		$.ajax({
+			  type: "POST",
+			  url: url,
+			  data: JSON.stringify(input_data),
+			  contentType: 'application/json',
+			  success: function(data)
+			  { 
+				  console.log(data);
+				  
+				  if(data == 'Authenticated') {
+					  //alert('Authenticated - go to the main page with the username');
+					  //window.location = 'http://localhost:8081/exploratory_analysis/main/' + username;
+					  window.location = 'http://' + EA.host + ':' + EA.port + '/exploratory_analysis/main/' + username;
+				  } else {
+					  console.log('Not authenticated - stay on page');
+					  $('#auth_notice').show();
+				  }
+				  
+			  },
+			  error: function(xhr, status, error) {
+				  console.log('error' + xhr); 
+			    if(xhr.status==404)
+			    { 
+			    	
+			    }
+			    
+			  }
+			  
+		});
+		
+		
+		
+		
+		
+		
+	});
+	
+});
