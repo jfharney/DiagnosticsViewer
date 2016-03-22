ab72fff416251738758e844ba93329c3b81c600d
Smith B E <bs1@acme-ea.ornl.gov>
2016-03-22 16:05:51 -0400

    added slashes in paths

--- django-app-1.8/ea/exploratory_analysis/amwg.py
+++ django-app-1.8/ea/exploratory_analysis/amwg.py
@@ -217,7 +217,7 @@ def pageGenerator(sets, varlist, times, package, dataset, options):
                                          varopts = amwgmaster.diags_collection[sets][v].get('varopts', False)
                                          pfixes = amwgmaster.diags_collection[sets][v].get('postfixes', False)
                                          if varopts == False and pfixes == False:
-                                            fname = 'http://' + ea_hostname + generate_token_url('/' + dataset + '/' + package + 'set'+sets+regionstr+seasonstr+'_'+v+'_'+obsfname+postfix)
+                                            fname = 'http://' + ea_hostname + generate_token_url('/' + dataset + '/' + package + '/set'+sets+regionstr+seasonstr+'_'+v+'_'+obsfname+postfix)
                                             click = 'onclick="' + javascript_namespace + 'displayImageClick(\''+fname+'\');" '
                                             over = 'onmouseover="' + javascript_namespace + 'displayImageHover(\''+fname+'\');" '
                                             out = 'onmouseout="' + javascript_namespace + 'nodisplayImage();" '
@@ -226,7 +226,7 @@ def pageGenerator(sets, varlist, times, package, dataset, options):
                                          else:
                                             if pfixes == False:
                                                for varopt in varopts:
-                                                  fname = 'http://' + ea_hostname + generate_token_url('/' + dataset + '/' + package + 'set'+sets+regionstr+seasonstr+'_'+v+'_'+varopt+'_'+obsfname+postfix)
+                                                  fname = 'http://' + ea_hostname + generate_token_url('/' + dataset + '/' + package + '/set'+sets+regionstr+seasonstr+'_'+v+'_'+varopt+'_'+obsfname+postfix)
                                                   click = 'onclick="' + javascript_namespace + 'displayImageClick(\''+fname+'\');" '
                                                   over = 'onmouseover="' + javascript_namespace + 'displayImageHover(\''+fname+'\');" '
                                                   out = 'onmouseout="' + javascript_namespace + 'nodisplayImage();" '
@@ -235,7 +235,7 @@ def pageGenerator(sets, varlist, times, package, dataset, options):
                                             else:
                                                 print 'VERIFY WHERE THE PFIX GOES ON FILES'
                                                 for pfix in pfixes:
-                                                  fname = 'http://' + ea_hostname + generate_token_url('/' + dataset + '/' + package + 'set'+sets+regionstr+seasonstr+'_'+v+'_'+pfix+'_'+obsfname+postfix)
+                                                  fname = 'http://' + ea_hostname + generate_token_url('/' + dataset + '/' + package + '/set'+sets+regionstr+seasonstr+'_'+v+'_'+pfix+'_'+obsfname+postfix)
                                                   click = 'onclick="' + javascript_namespace + 'displayImageClick(\''+fname+'\');" '
                                                   over = 'onmouseover="' + javascript_namespace + 'displayImageHover(\''+fname+'\');" '
                                                   out = 'onmouseout="' + javascript_namespace + 'nodisplayImage();" '
@@ -260,7 +260,7 @@ def pageGenerator(sets, varlist, times, package, dataset, options):
                 html+='<TH>'+r+'</TH>'
                 for season in seasons:
                      #whenever these are paths.generated with diags use this?
-                     fname = 'http://' + ea_hostname + generate_token_url('/' + dataset + '/' + package + 'set1_'+season+'_'+amwgmaster.all_regions[r].filekey+'-table.text')
+                     fname = 'http://' + ea_hostname + generate_token_url('/' + dataset + '/' + package + '/set1_'+season+'_'+amwgmaster.all_regions[r].filekey+'-table.text')
         #             print 'looking for '+fname
                      click = 'onclick="' + javascript_namespace + 'displayTable(\''+fname+'\');" '
                      over = 'onmouseover="' + javascript_namespace + 'displayTableHover(\''+fname+'\');" '
@@ -341,9 +341,9 @@ def pageGenerator(sets, varlist, times, package, dataset, options):
             
                for col in cols:
                   if sets == '13':
-                     fname = 'http://' + ea_hostname + generate_token_url('/' + dataset + '/' + package  + 'set13_' + col + '_' + vlist[var] + '.png')
+                     fname = 'http://' + ea_hostname + generate_token_url('/' + dataset + '/' + package  + '/set13_' + col + '_' + vlist[var] + '.png')
                   else:
-                     fname = 'http://' + ea_hostname + generate_token_url('/' + dataset + '/' + package  + 'set12_' + vlist[var] + '_' + col + '.png')
+                     fname = 'http://' + ea_hostname + generate_token_url('/' + dataset + '/' + package  + '/set12_' + vlist[var] + '_' + col + '.png')
     
                   click = 'onclick="' + javascript_namespace + 'displayImageClick(\''+fname+'\');" '
                   over = 'onmouseover="' + javascript_namespace + 'displayImageHover(\''+fname+'\');" '
